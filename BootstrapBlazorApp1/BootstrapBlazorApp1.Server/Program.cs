a80c4cbdea63def8e85663d669112872279590a2
James (趙福春) Chao <James.Chao@quantatw.com>
2024-05-07 10:46:33 +0800

    Add project files.

---
+++ BootstrapBlazorApp1/BootstrapBlazorApp1.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using BootstrapBlazorApp1.Server.Components;
+using Microsoft.AspNetCore.SignalR;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddBootstrapBlazor();
+
+// 增加 SignalR 服务数据传输大小限制配置
+builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseResponseCompression();
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
