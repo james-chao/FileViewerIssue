3fa9c832723e7cb03b1bc41a1297d694756b68e5
Argo-AscioTech <argo@live.ca>
2024-05-08 16:01:32 +0800

    fix: add Html2Pdf service

--- BootstrapBlazorApp1/BootstrapBlazorApp1.Server/Program.cs
+++ BootstrapBlazorApp1/BootstrapBlazorApp1.Server/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddBootstrapBlazor();
 // 增加 Table Excel 导出服务
 builder.Services.AddBootstrapBlazorTableExportService();
 
+builder.Services.AddBootstrapBlazorHtml2PdfService();
+
 // 增加 SignalR 服务数据传输大小限制配置
 builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
 
